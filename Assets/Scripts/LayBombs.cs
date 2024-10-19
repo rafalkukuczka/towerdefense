011d0c2ad78aae860d2e28cb18d9d579373803a1
rkukuczka <rafal.qqczka@gmail.com>
2024-10-19 15:24:40 +0200

    Init

---
+++ Assets/Scripts/LayBombs.cs
@@ -0,0 +1,60 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.InputSystem;
+
+public class LayBombs : MonoBehaviour
+{
+	[HideInInspector]
+	public bool bombLaid = false;		// Whether or not a bomb has currently been laid.
+	public int bombCount = 0;			// How many bombs the player has.
+	public AudioClip bombsAway;			// Sound for when the player lays a bomb.
+	public GameObject bomb;				// Prefab of the bomb.
+
+
+	private UnityEngine.UI.Image bombHUD;           // Heads up display of whether the player has a bomb or not.
+
+	PlayerActionsExample playerInput;
+
+	void Awake ()
+	{
+		// Setting up the reference.
+		// TODO RKbombHUD = GameObject.Find("ui_bombHUD").guiTexture;
+		playerInput = new PlayerActionsExample ();
+	}
+
+
+	void Update ()
+	{
+		// If the bomb laying button is pressed, the bomb hasn't been laid and there's a bomb to lay...
+		//RK New input
+		//if(Input.GetButtonDown("Fire2") && !bombLaid && bombCount > 0)
+		if (playerInput.Player.Fire2.triggered)
+		{
+			// Decrement the number of bombs.
+			bombCount--;
+
+			// Set bombLaid to true.
+			bombLaid = true;
+
+			// Play the bomb laying sound.
+			AudioSource.PlayClipAtPoint(bombsAway,transform.position);
+
+			// Instantiate the bomb prefab.
+			Instantiate(bomb, transform.position, transform.rotation);
+		}
+
+		// The bomb heads up display should be enabled if the player has bombs, other it should be disabled.
+		// TODO RK bombHUD.enabled = bombCount > 0;
+	}
+
+
+    private void OnEnable()
+    {
+        playerInput.Enable();
+    }
+
+    private void OnDisable()
+    {
+        playerInput.Disable();
+    }
+}
---
+++ Assets/Scripts/Pauser.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Pauser : MonoBehaviour {
+	private bool paused = false;
+
+
+	//RK New Input
+	PlayerActionsExample playerInput;
+
+    private void Awake()
+    {
+		playerInput = new PlayerActionsExample();
+    }
+
+
+    // Update is called once per frame
+    void Update () {
+		//RK New input
+		//if(Input.GetKeyUp(KeyCode.P))
+		if (playerInput.Player.Pause.triggered)
+		{
+			paused = !paused;
+		}
+
+		if(paused)
+			Time.timeScale = 0;
+		else
+			Time.timeScale = 1;
+	}
+
+    private void OnEnable()
+    {
+        playerInput.Enable();
+    }
+
+    private void OnDisable()
+    {
+        playerInput.Disable();
+    }
+}
