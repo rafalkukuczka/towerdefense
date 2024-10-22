c439220e313b7a0dce9c27aa7f22e94f1b8929ef
rkukuczka <rafal.qqczka@gmail.com>
2024-10-22 11:56:48 +0200

    Making LevelSelector object oriented

---
+++ Assets/Scripts/LevelSelectorController.cs
@@ -0,0 +1,89 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using static System.Net.Mime.MediaTypeNames;
+
+[ExecuteAlways]
+public class LevelSelectorController : MonoBehaviour
+{
+    public Sprite ButtonSprite;
+
+    public string SceneName;
+    public string SceneTitle;
+
+    Button _button;
+    UnityEngine.UI.Image _image;
+    TextMeshProUGUI _textBox;
+    // Start is called before the first frame update
+    void Awake()
+    {
+        _textBox = this.gameObject.transform.GetChild(0).GetComponent<TextMeshProUGUI>();
+        _image = this.gameObject.transform.GetChild(1).GetComponent<UnityEngine.UI.Image>();
+        _button = this.gameObject.transform.GetComponent<Button>();
+
+        _textBox.text = SceneTitle;
+
+        if (ButtonSprite != null )
+            _image.sprite = ButtonSprite;
+
+        
+    }
+
+    private void OnValidate()
+    {
+        var textBox = this.gameObject.transform.GetChild(0).GetComponent<TextMeshProUGUI>();
+        textBox.text = SceneTitle;
+
+        var image = this.gameObject.transform.GetChild(1).GetComponent<UnityEngine.UI.Image>();
+
+        if (ButtonSprite != null)
+            image.sprite = ButtonSprite;
+
+
+    }
+
+    // Update is called once per frame
+
+    public void OnSelect()
+    {
+        StartCoroutine(OnClick());
+    }
+
+    IEnumerator OnClick() {
+
+        _image.color = Color.green;
+
+        yield return new WaitForSeconds(1);
+
+        _image.color = Color.white;
+
+        yield return new WaitForSeconds(1);
+
+        SceneManager.LoadScene(SceneName);
+
+        yield return null;
+    }
+
+    public void SetActive(bool state)
+    {
+        
+
+        if (state)
+        {
+            _textBox.color = Color.white;
+            _image.color = Color.white;
+            _button.interactable = true;
+        }
+        else
+        {
+            _textBox.color = Color.gray;
+            _image.color = Color.grey;
+            _button.interactable = false;
+        }
+
+     
+    }
+}
---
+++ Assets/Scripts/MenuGridController.cs
@@ -0,0 +1,54 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuGridController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        SetActive(0, true); 
+
+        if (GameData.Score > 1000)
+        {
+            SetActive(1, true);
+        }
+        else
+        {
+            SetActive(1, false);
+        }
+
+        if (GameData.Score > 2000)
+        {
+            SetActive(2, true);
+        }
+        else
+        {
+            SetActive(2, false);
+        }
+
+        if (GameData.Score > 3000)
+        {
+            SetActive(3, true);
+        }
+        else
+        {
+            SetActive(3, false);
+        }
+
+        //RK DEBUG SetActive(4, true);
+    }
+
+    private void SetActive(int idx, bool isActive)
+    {
+        var levelSelector = this.transform.GetChild(idx).GetComponent<LevelSelectorController>();
+        levelSelector.SetActive(isActive);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
