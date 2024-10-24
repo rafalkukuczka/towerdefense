24d597153d10485c1deab5f42bef887a3b273d5e
rkukuczka <rafal.qqczka@gmail.com>
2024-10-24 10:48:13 +0200

    Cleanup

--- Assets/Scripts/LayBombs.cs
+++ Assets/Scripts/LayBombs.cs
@@ -23,20 +23,17 @@ public class LayBombs : MonoBehaviour
 		// Setting up the reference.
 		// TODO RK
 		var bombHUD = GameObject.Find("ui_bombHUD");
-		if (bombHUD != null)
-		{
-			bombHUDImage = bombHUD.gameObject.GetComponent<Image>();
-		}
-		playerInput = new PlayerActionsExample ();
+		bombHUDImage = bombHUD.gameObject.GetComponent<Image>();
+
+        playerInput = new PlayerActionsExample ();
 	}
 
 
 	void Update ()
 	{
         // If the bomb laying button is pressed, the bomb hasn't been laid and there's a bomb to lay...
-        //RK New input
+        //RK New input & debug support
         //if(Input.GetButtonDown("Fire2") && !bombLaid && bombCount > 0)
-        //RK DEBUG unlimited bombs if (playerInput.Player.Fire2.triggered)
         if ((playerInput.Player.Fire2.triggered && ((GameData.IsUnlimimtedBombs() ) || !bombLaid && bombCount > 0)))
         {
 			// Decrement the number of bombs.
@@ -52,12 +49,9 @@ public class LayBombs : MonoBehaviour
 			Instantiate(bomb, transform.position, transform.rotation);
 		}
 
-		// if exists  The bomb heads up display should be enabled if the player has bombs, other it should be disabled.
-		if (bombHUDImage != null)
-		{
-			bombHUDImage.enabled = bombCount > 0;
-		}
-	}
+        //The bomb heads up display should be enabled if the player has bombs, other it should be disabled.
+        bombHUDImage.enabled = bombCount > 0;
+    }
 
 
     private void OnEnable()
